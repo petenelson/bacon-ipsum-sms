9d7fae9d99b88531ed117f42a7c5e8c1df7e79dd
Pete Nelson <pete@petenelson.com>
2016-11-16 14:28:15 -0500

    initial work on admin settings

--- includes/admin.php
+++ includes/admin.php
@@ -4,4 +4,34 @@ namespace Bacon_Ipsum\SMS\Admin;
 
 function setup() {
 
+	add_action( 'admin_init', __NAMESPACE__ . '\register_settings' );
+
+}
+
+function register_settings() {
+
+	register_setting( 'general', 'bacon-ipsum-sms-twilio-sid',   'sanitize_text_field' );
+	register_setting( 'general', 'bacon-ipsum-sms-twilio-token', 'sanitize_text_field' );
+
+	add_settings_section( 'bacon-ipsum-sms', 'Bacon Ipsum SMS', null, 'general' );
+
+	add_settings_field( 'bacon-ipsum-sms-twilio-sid',
+		'Twilio Phone SID', __NAMESPACE__ . '\sid_input', 'general', 'bacon-ipsum-sms' );
+
+	add_settings_field( 'bacon-ipsum-sms-twilio-token',
+		'Twilio Token', __NAMESPACE__ . '\token_input', 'general', 'bacon-ipsum-sms' );
+}
+
+function sid_input() {
+	$value = get_option( 'bacon-ipsum-sms-twilio-sid' );
+	?>
+		<input type="text" name="bacon-ipsum-sms-twilio-sid" value="<?php echo esc_attr( $value ); ?>" class="regular-text" />
+	<?php
+}
+
+function token_input() {
+	$value = get_option( 'bacon-ipsum-sms-twilio-token' );
+	?>
+		<input type="text" name="bacon-ipsum-sms-twilio-token" value="<?php echo esc_attr( $value ); ?>" class="regular-text" />
+	<?php
 }
--- includes/rest-api.php
+++ includes/rest-api.php
@@ -4,4 +4,35 @@ namespace Bacon_Ipsum\SMS\REST_API;
 
 function setup() {
 
+	add_action( 'rest_api_init', __NAMESPACE__ . '\add_sms_endpoint' );
+
+}
+
+function add_sms_endpoint() {
+	register_rest_route(
+		'bacon-ipsum',
+		'v1/sms',
+		array(
+			'callback'      => __NAMESPACE__ . '\handle_sms_request',
+			'methods'       => array( 'GET', 'POST' ),
+			'args'          => array(
+				),
+
+			)
+	);
+
+	// TODO add a voice reply option
+
+}
+
+function handle_sms_request() {
+
+	$filler = apply_filters( 'anyipsum-generate-filler', array(
+		'number-of-sentences' => 3,
+		'max-number-of-paragraphs' => 1,
+		'start-with-lorem' => 1,
+		)
+	);
+
+	return rest_ensure_response( $filler );
 }
